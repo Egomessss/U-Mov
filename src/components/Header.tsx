88670e25d22b67ee9a1fa7a43d177d93be1cb8d0
Egomessss <ed.gomes97@gmail.com>
2023-03-06 18:06:10 +0000

    bullets hooks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { AiFillStar, AiOutlineArrowRight } from "react-icons/ai";
+import { AiFillBook, AiFillStar, AiOutlineArrowRight, AiOutlineSearch } from "react-icons/ai";
+import { MdCompare } from "react-icons/md";
 
 
 function Header() {
@@ -26,34 +27,35 @@ function Header() {
 
             </nav>
             {/* Main  */}
-            <main>
+            <main className='p-20 flex-col flex gap-20'>
                 {/* hero */}
-                <div className='text-cream h-[800px] p-20 flex gap-10'><div className='w-2/4'>
-                    {/* text */}
-                    <div className='h-3/4 flex flex-col gap-12'>
-                        <h3>Moving has never been easier</h3>
-                        <h1 className='text-7xl font-lilita uppercase'>Giving You Your time Back</h1>
-                        <p>Everything you need to know before you move to your dream house in your fingertips</p>
-                    </div>
-
-                    <div className='flex items-center gap-10 '>
-
-                        <div>
-                            <p>+465 users</p>
-                            <div className='flex text-yellow'>
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                            </div>
+                <div className='text-cream h-[600px] flex gap-10'>
+                    <div className='w-2/4'>
+                        {/* text */}
+                        <div className='h-3/4 flex flex-col gap-12'>
+                            <h3>Moving has never been easier</h3>
+                            <h1 className='text-7xl font-lilita uppercase'>Giving You Your time Back</h1>
+                            <p>Everything you need to know before you move to your dream house in your fingertips</p>
                         </div>
-                        <button className='text-black font-lilita gap-2 flex justify-center h-[50px] font-bold items-center text-2xl bg-yellow rounded-md w-[250px] uppercase'>
-                            <p>Try u-Mov Free</p>
-                            <AiOutlineArrowRight />
-                        </button>
+
+                        <div className='flex items-center gap-10 '>
+
+                            <div>
+                                <p>+465 users</p>
+                                <div className='flex text-yellow'>
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                    <AiFillStar />
+                                </div>
+                            </div>
+                            <button className='text-black font-lilita gap-2 flex justify-center h-[50px] font-bold items-center text-2xl bg-yellow rounded-md w-[250px] uppercase'>
+                                <p>Try u-Mov Free</p>
+                                <AiOutlineArrowRight />
+                            </button>
+                        </div>
                     </div>
-                </div>
                     <div className='w-2/4'>
                         {/* image */}
                         <img className='rounded-xl object-cover w-full h-full' src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5fce34b954f89982650812a2_Layer%2020%20(1).jpg" alt="" />
@@ -61,13 +63,32 @@ function Header() {
 
 
                 {/* hook */}
-                <div className='flex justify-center flex-col items-center'>
+                <div className='flex justify-center flex-col items-center gap-10'>
                     <h2 className='text-5xl font-lilita'>All the advantages</h2>
                     {/* bullet hooks */}
-                    <ul>
-                        <li>Compare</li>
-                        <li>Search</li>
-                        <li>Research</li>
+                    <ul className='flex justify-between w-full'>
+                        <li>
+                            <div className='flex gap-2 items-center'> <MdCompare />
+                                <h3 className='text-2xl font-bold'>Compare</h3></div>
+
+                            <p>Compare everything side by side and make the decision that suits you the best
+                            </p>
+                        </li>
+                        <li>
+                            <div className='flex gap-2 items-center'>
+                                <AiOutlineSearch />
+                                <h3 className='text-2xl font-bold'>Search</h3></div>
+
+                            <p>You dont know the location? use our extension
+                            </p>
+                        </li>
+                        <li>
+                            <div className='flex gap-2 items-center'>
+                                <AiFillBook />
+                                <h3 className='text-2xl font-bold'>Research</h3></div>
+                            <p>Research every fact before you move
+                            </p>
+                        </li>
                     </ul>
                 </div>
 
