88e41d812f8967fb29422c95db8998f1d536ceb5
Egomessss <ed.gomes97@gmail.com>
2023-03-09 03:24:17 +0000

    prettier is now working

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,63 +1,72 @@
-import React from 'react'
-import { AiFillStar, AiOutlineArrowRight } from 'react-icons/ai'
+import React from "react"
+import { AiFillStar, AiOutlineArrowRight } from "react-icons/ai"
 
 function Hero() {
-    return (
-        <div>
-            {/* full */}
-            <div className='dark:text-[#EEEEEE] hidden h-[600px] md:flex gap-10'>
-                <div className='w-2/4'>
-                    {/* text */}
-                    <div className='h-3/4 flex flex-col gap-12'>
-                        <h3 className='font-bold text-xl'>Moving has never been easier</h3>
-                        <h1 className='text-7xl font-lilita uppercase'>Giving You Your time Back</h1>
-                        <p className='font-bold'>Everything you need to know before you move to your dream house in your fingertips</p>
-                    </div>
+  return (
+    <div>
+      {/* full */}
+      <div className="hidden h-[600px] gap-10 dark:text-[#EEEEEE] md:flex">
+        <div className="w-2/4">
+          {/* text */}
+          <div className="flex h-3/4 flex-col gap-12">
+            <h3 className="text-xl font-bold">Moving has never been easier</h3>
+            <h1 className="font-lilita text-7xl uppercase">
+              Giving You Your time Back
+            </h1>
+            <p className="font-bold">
+              Everything you need to know before you move to your dream house in
+              your fingertips
+            </p>
+          </div>
 
-                    <div className='flex items-center gap-10 '>
-
-                        <div>
-                            <p>+465 users</p>
-                            <div className='flex dark:text-orange'>
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                                <AiFillStar />
-                            </div>
-                        </div>
-                        <button className='dark:text-black font-lilita gap-2 flex justify-center h-[50px] font-bold items-center text-2xl bg-orange rounded-md w-[250px] uppercase'>
-                            <p>Try u-Mov Free</p>
-                            <AiOutlineArrowRight />
-                        </button>
-                    </div>
-                </div>
-                <div className='w-2/4'>
-                    {/* image */}
-                    <img className='rounded-xl object-cover w-full h-full' src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5fce34b954f89982650812a2_Layer%2020%20(1).jpg" alt="" />
-                </div>
+          <div className="flex items-center gap-10 ">
+            <div>
+              <p>+465 users</p>
+              <div className="flex dark:text-orange">
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+              </div>
             </div>
-            <div className='flex justify-center items-center flex-col md:hidden '>
-                <div className='dark:text-[#EEEEEE] h-[400px] py-10'>
-                    {/* image */}
-                    <img className='rounded-xl object-cover w-full h-full'
-                        src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5fce34b954f89982650812a2_Layer%2020%20(1).jpg" alt="" />
-                </div>
-                {/* mobile */}
-                <div className='flex justify-center items-center flex-col gap-5'>
-                    <h1 className='font-bold text-3xl text-center'>Moving has never been easier</h1>
-                    <button className='dark:text-black  font-lilita gap-2 flex justify-center h-[50px] font-bold items-center text-2xl bg-orange rounded-md w-[250px] uppercase'>
-                        <p>Try u-Mov Free</p>
-                        <AiOutlineArrowRight />
-                    </button>
-                </div>
-
-            </div>
+            <button className="flex h-[50px] w-[250px] items-center justify-center gap-2 rounded-md bg-orange font-lilita text-2xl font-bold uppercase dark:text-black">
+              <p>Try u-Mov Free</p>
+              <AiOutlineArrowRight />
+            </button>
+          </div>
         </div>
-
-
-
-    )
+        <div className="w-2/4">
+          {/* image */}
+          <img
+            className="h-full w-full rounded-xl object-cover"
+            src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5fce34b954f89982650812a2_Layer%2020%20(1).jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center md:hidden ">
+        <div className="h-[400px] py-10 dark:text-[#EEEEEE]">
+          {/* image */}
+          <img
+            className="h-full w-full rounded-xl object-cover"
+            src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5fce34b954f89982650812a2_Layer%2020%20(1).jpg"
+            alt=""
+          />
+        </div>
+        {/* mobile */}
+        <div className="flex flex-col items-center justify-center gap-5">
+          <h1 className="text-center text-3xl font-bold">
+            Moving has never been easier
+          </h1>
+          <button className="flex  h-[50px] w-[250px] items-center justify-center gap-2 rounded-md bg-orange font-lilita text-2xl font-bold uppercase dark:text-black">
+            <p>Try u-Mov Free</p>
+            <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
