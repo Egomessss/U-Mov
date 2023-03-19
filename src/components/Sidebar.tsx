31450e22644fc71273ad1aa5f3d3f9aa9340a4aa
Egomessss <111806970+Egomessss@users.noreply.github.com>
2023-03-19 21:41:22 +0000

    sidebar of the sidebar added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,31 +4,109 @@ import React, { Fragment, useState } from "react"
 
 function Sidebar() {
   return (
-    <div className="absolute top-1/2 left-10 flex h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-around rounded-3xl border-2 border-white">
-      <Popover className="relative">
-        {({ open }) => (
-          <>
-            <Popover.Button>Directions</Popover.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute bg-white left-24 w-52 h-64"></Popover.Panel>
-            </Transition>
-          </>
-        )}
-      </Popover>
-      <div></div>
-      <div className="h-20 w-20 rounded-full border-2 border-white"></div>
-      <div className="h-20 w-20 rounded-full border-2 border-white"></div>
-      <div className="h-20 w-20 rounded-full border-2 border-white"></div>
-      <div className="h-20 w-20 rounded-full border-2 border-white"></div>
-      <div className="h-20 w-20 rounded-full border-2 border-white"></div>
+    <div className="absolute top-1/2 left-10 h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform  rounded-3xl border-2 border-white">
+      <div className="relative flex h-full w-full flex-col items-center justify-around">
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+                Directions
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-x-1"
+                enterTo="opacity-100 translate-x-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-1"
+              >
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl bg-white"></Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+                Directions
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-x-1"
+                enterTo="opacity-100 translate-x-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-1"
+              >
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl bg-white"></Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+                Directions
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-x-1"
+                enterTo="opacity-100 translate-x-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-1"
+              >
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl bg-white"></Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+                Directions
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-x-1"
+                enterTo="opacity-100 translate-x-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-1"
+              >
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl bg-white"></Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
+        <Popover>
+          {({ open }) => (
+            <>
+              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+                Directions
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-x-1"
+                enterTo="opacity-100 translate-x-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 translate-x-1"
+              >
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl bg-white"></Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
+      </div>
     </div>
   )
 }
