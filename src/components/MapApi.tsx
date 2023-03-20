b9e15e5d27365141506cff6d4883d92c188e4407
Egomessss <111806970+Egomessss@users.noreply.github.com>
2023-03-20 23:06:51 +0000

    google maps api added

--- src/components/MapApi.tsx
+++ src/components/MapApi.tsx
@@ -1,7 +1,34 @@
 import React from "react"
+import { GoogleMap, LoadScript } from "@react-google-maps/api"
+
+//  Make sure you cache the props passed to GoogleMap to avoid re-renders that may harm the performance.
+// restringir a api somente para o site
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+}
 
 function MapApi() {
-  return <div className="-mx-20  h-full border-2 border-white"></div>
+  return (
+    <div className="-mx-20  h-full border-2 border-white">
+      <LoadScript googleMapsApiKey="AIzaSyAC-ZmHeOUM6VvIDtbc8y_sfKG-Lh7ZgME">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  )
 }
 
 export default MapApi
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import React, { Fragment, useState } from "react"
 
 function Sidebar() {
   return (
-    <div className="absolute top-1/2 left-10 h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform  rounded-3xl border-2 border-white">
+    <div className="absolute z-50 top-1/2 left-5 bg-black/60 h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform  rounded-3xl border-2 border-white">
       <div className="relative flex h-full w-full flex-col items-center justify-around">
         {/* directions */}
         <Popover>
@@ -22,11 +22,19 @@ function Sidebar() {
                 leaveFrom="opacity-100 translate-x-0"
                 leaveTo="opacity-0 translate-x-1"
               >
-                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl border-2 border-white p-4">
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl border-2 border-white p-4 bg-black/60">
                   <div className="flex flex-col gap-2">
                     <div className="flex flex-col">
                       {" "}
                       <h3>Add main location</h3>
+                      <input type="text" />
+                    </div>
+                    <button>Add more</button>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex flex-col">
+                      {" "}
+                      <h3>Add middle routes</h3>
                       <input type="text" />
                     </div>
                     <button>Add more</button>
@@ -38,6 +46,15 @@ function Sidebar() {
                       <input type="text" />
                     </div>
                     <button>Add more</button>
+                  </div>
+                  <h1>Summary</h1>
+                  <div className="flex flex-col gap-2">
+                    <h3>Distance</h3>
+                    <div className="flex justify-between">
+                      <span>Point A</span>
+                      <span>distance</span>
+                      <span>Point b</span>
+                    </div>
                   </div>
                 </Popover.Panel>
               </Transition>
