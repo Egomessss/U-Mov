009479177f881c48a3f8eaf2022c49f37a1563d6
Egomessss <ed.gomes97@gmail.com>
2023-03-27 17:46:07 +0100

    fixed spacing in directions sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,36 @@
 import { Popover, Transition } from "@headlessui/react"
 import {
   ArrowRightIcon,
+  ClockIcon,
   ViewfinderCircleIcon,
 } from "@heroicons/react/24/outline"
 import React, { Fragment, useState } from "react"
 import { Autocomplete } from "@react-google-maps/api"
 
-function Sidebar({ centerMap, originRef, destinationRef, calculateRoute, clearRoute, duration, distance  }) {
+function Sidebar({
+  centerMap,
+  originRef,
+  destinationRef,
+  calculateRoute,
+  clearRoute,
+  duration,
+  distance,
+}) {
   return (
-    <div className="absolute top-1/2 left-5 z-50 h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-3xl  border-2 border-white bg-black/60">
+    <div className="absolute top-1/2 left-5 z-50 h-[80%] w-24 -translate-x-1/2 -translate-y-1/2 transform rounded-3xl  border-2 border-white bg-white ">
       <div className="relative flex h-full w-full flex-col items-center justify-around">
         {/* centers the map to main location */}
         <button
           onClick={centerMap}
-          className="flex h-20 w-20 items-center justify-center rounded-full border-2 border-white"
+          className="flex h-20 w-20 items-center justify-center rounded-full border-2 bg-lightgray"
         >
-          <ViewfinderCircleIcon className="h-8" />
+          <ViewfinderCircleIcon className="h-8 text-orange" />
         </button>
         {/* directions */}
         <Popover>
           {({ open }) => (
             <>
-              <Popover.Button className="h-20 w-20 rounded-full border-2 border-white">
+              <Popover.Button className="h-20 w-20 rounded-full  bg-lightgray text-orange">
                 Directions
               </Popover.Button>
               <Transition
@@ -33,22 +42,29 @@ function Sidebar({ centerMap, originRef, destinationRef, calculateRoute, clearRo
                 leaveFrom="opacity-100 translate-x-0"
                 leaveTo="opacity-0 translate-x-1"
               >
-                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl border-2 border-white bg-black/60 p-4">
+                <Popover.Panel className="absolute left-24 top-0 h-full w-96 rounded-3xl  bg-white p-4 text-black">
                   <div className="flex flex-col gap-2">
                     <div className="flex flex-col">
-                      {" "}
                       <h3>Add main location</h3>
                       <Autocomplete>
-                        <input type="text" placeholder="Origin" ref={originRef} />
+                        <input
+                          className="w-full rounded-md border-2 bg-white px-2"
+                          type="text"
+                          placeholder="Origin"
+                          ref={originRef}
+                        />
                       </Autocomplete>
                     </div>
                     <button>Add more</button>
                   </div>
                   <div className="flex flex-col gap-2">
                     <div className="flex flex-col">
-                      {" "}
-                      <h3>Add middle routes</h3>
-                      <input type="text" placeholder="middle" />
+                      <h3>Add a deviation</h3>
+                      <input
+                        className="w-full rounded-md border-2 bg-white px-2"
+                        type="text"
+                        placeholder="Deviations"
+                      />
                     </div>
                     <button>Add more</button>
                   </div>
@@ -57,21 +73,33 @@ function Sidebar({ centerMap, originRef, destinationRef, calculateRoute, clearRo
                     <div className="flex flex-col">
                       <label htmlFor="">Give it a name</label>
                       <Autocomplete>
-                      <input type="text" placeholder="Origin" ref={destinationRef} />
+                        <input
+                          className="w-full rounded-md border-2 bg-white px-2"
+                          type="text"
+                          placeholder="Origin"
+                          ref={destinationRef}
+                        />
                       </Autocomplete>
                     </div>
                     <button>Add more</button>
                   </div>
-                  <button onClick={calculateRoute}>Get Route</button>
-                  <button onClick={clearRoute}>Clear selection</button>
-                  <h1>Summary</h1>
+                  <div className="flex justify-between py-6">
+                    {" "}
+                    <button onClick={calculateRoute}>Get Route</button>
+                    <button onClick={clearRoute}>Clear selection</button>
+                  </div>
+
+                  <h3>Summary</h3>
                   <div className="flex flex-col gap-2">
-                    <h3>Distance</h3>
                     <div className="flex justify-between">
-                      <span>Point A</span>
-                      <span>distance: {distance}</span>
-                      <span>duration: {duration}</span>
-                      <span>Point b</span>
+                      <span>House</span>
+                      <ArrowRightIcon className="h-6" />
+                      <span>Job</span>
+                      <span>{distance}</span>
+                      <div className="flex gap-2">
+                        <ClockIcon className="h-6" />
+                        <span>{duration} </span>
+                      </div>
                     </div>
                   </div>
                 </Popover.Panel>
